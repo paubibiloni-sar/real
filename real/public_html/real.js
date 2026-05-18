a86ec80e17a5683f0eca1e9f8b6beac177d948b0
PID <PID@DESKTOP-PBTOS8R>
2026-05-18 13:03:33 +0200

    He creat les variables per poder continuar programant

--- real/public_html/real.js
+++ real/public_html/real.js
@@ -1,6 +1,10 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+"use_strict";
+let vidaJugador = 100;
+let decisioJugador = "";
+let escenaActual = 1;
+let objectesInventari = [];
+
+alert("Benvingut a La Cova Misteriosa!");
+
 
 
