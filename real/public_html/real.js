9b43dcdffa8b368c2a42e99d6be352b5a2ebb7f4
PID <PID@DESKTOP-PBTOS8R>
2026-05-20 11:58:27 +0200

    Hem acabat el codi del joc

--- real/public_html/real.js
+++ real/public_html/real.js
@@ -1,10 +1,71 @@
-"use_strict";
 let vidaJugador = 100;
 let decisioJugador = "";
-let escenaActual = 1;
 let objectesInventari = [];
 
-alert("Benvingut a La Cova Misteriosa!");
+alert("Benvingut a l'aventura!");
 
+// Vols jugar?
+decisioJugador = prompt("Vols jugar? (si/no)");
 
+if (decisioJugador == "no") {
 
+    alert("Fi del joc");
+
+} else {
+
+    // Escollir entre riu o cova
+    decisioJugador = prompt(
+        "Vols entrar al riu o a la cova?"
+    );
+
+    // CAMÍ DEL RIU
+    if (decisioJugador == "riu") {
+
+        alert("Trobes un os super agressiu");
+
+        decisioJugador = prompt(
+            "Vols lluitar o correr per la teva vida?"
+        );
+
+        if (decisioJugador == "lluitar") {
+
+            alert("Perds, te menja sencera");
+            vidaJugador = 0;
+
+        } else {
+
+            alert("Has escapat!");
+            alert("Has trobat la teva tribu, GUANYES");
+        }
+
+    // CAMÍ DE LA COVA
+    } else {
+
+        alert("Trobes un ós i has de lluitar");
+
+        decisioJugador = prompt(
+            "Vols donar-li un cop de puny o correr?"
+        );
+
+        if (decisioJugador == "cop de puny") {
+
+            alert("Mates l'ós");
+            alert("Has trobat la teva tribu, GUANYES");
+
+        } else {
+
+            alert("L'ós et mata");
+            vidaJugador = 0;
+        }
+    }
+}
+
+// Final del joc
+if (vidaJugador <= 0) {
+
+    alert("Mors");
+
+} else {
+
+    alert("Final bo");
+}
