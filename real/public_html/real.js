8a5a84b69d6d460de5c43e62406721e8a59a2e84
PID <PID@DESKTOP-GB0HFOC>
2026-05-22 09:07:09 +0200

    llauna

--- real/public_html/real.js
+++ real/public_html/real.js
@@ -1,6 +1,64 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+"use_strict";
+let vidaJugador = 100;
+let decisioJugador = "";
+let objectesInventari = [];
 
+alert("Benvingut a l'aventura!");
 
+// Vols jugar?
+decisioJugador = prompt("Vols jugar? (si/no)");
+
+if (decisioJugador == "no") {
+
+    alert("Fi del joc");
+
+} else {
+
+    // Escollir entre riu o cova
+    decisioJugador = prompt("Vols entrar al riu o a la cova?");
+
+    // CAMÍ DEL RIU
+    if (decisioJugador == "riu") {
+
+        alert("Trobes un os super agressiu");
+
+        decisioJugador = prompt("Vols lluitar o correr per la teva vida?");
+
+        if (decisioJugador == "lluitar") {
+
+            alert("Perds, te menja sencera");
+            vidaJugador = 0;
+
+        } else {
+
+            alert("Has escapat!");
+            alert("Has trobat la teva tribu, GUANYES");
+        }
+    } else {
+
+        alert("Trobes un ós i has de lluitar");
+
+        decisioJugador = prompt(
+            "Vols donar-li un cop de puny o correr?"
+        );
+
+        if (decisioJugador == "cop de puny") {
+
+            alert("Mates l'ós");
+            alert("Has trobat la teva tribu, GUANYES");
+
+        } else {
+
+            alert("L'ós et mata");
+            vidaJugador = 0;
+        }
+    }
+}
+
+if (vidaJugador <= 0) {
+
+    alert("Mors tu");
+} else {
+
+    alert("Final bo");
+}
